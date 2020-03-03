c5deb9ca66f8e7fdba59ae03478d9fdaa61630f6
tryhardsnipehrd <57113411+tryhardsnipehrd@users.noreply.github.com>
2020-03-03 16:31:03 -0700

    added typing

--- Sheriff.py
+++ Sheriff.py
@@ -24,41 +24,52 @@ async def on_message(message):
 
     
     if message.content.startswith('$invite'):
+        await ctx.trigger_typing()
         await message.channel.send('https://discordapp.com/oauth2/authorize?client_id=668932488068071427&scope=bot&permissions=8')
    
     if message.content.startswith('$oops'):
+        await ctx.trigger_typing()
         await message.channel.send('What did you do this time...')
 
     if 'oof' in message.content:
+        await ctx.trigger_typing()
         await message.channel.send("Big OOF")
 
     if 'stop' in message.content:
+        await ctx.trigger_typing()
         await message.channel.send("YOU NEED TO STOP IF THEY SAY TO!!!!")
 
     if message.content.startswith('$optimist'):
         await message.author.send(random.choice(happy))
     
     async def joined(ctx, member: discord.Member):
+        await ctx.trigger_typing()
         await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
 
     if 'sheriff' in message.content:
+        await ctx.trigger_typing()
         await message.channel.send("What do you want?? He is busy right now")
 
     if 'Yuri' in message.content:
+        await ctx.trigger_typing()
         await message.channel.send("DON'T MESS WITH THE SHERIFF'S WAI- I MEAN FRIEND...")
    
     if 'yuri' in message.content:
+        await ctx.trigger_typing()
         await message.channel.send("DON'T MESS WITH THE SHERIFF'S WAI- I MEAN FRIEND...")
 
     if message.content.startswith('$doki'):
+        await ctx.trigger_typing()
         await message.channel.send("use Y_invite, N_invite, S_invite, M_invite, and MC_invite to add all of the bots to your server!!! (PS... I would not recommend Monika... She is a bitch)")
 
     if message.content.startswith('$color'):
+        await ctx.trigger_typing()
         await message.channel.send('Here are three numbers for RGB colors...')
         await message.channel.send('3')
         await message.channel.send('2')
         await message.channel.send('1')
         await message.channel.send('Here they are!!!!')
+        await ctx.trigger_typing()
         thing = random.randint(1, 255)
         await message.channel.send(thing)
         thing = random.randint(1, 255)
@@ -108,26 +119,31 @@ async def on_message(message):
 
 @bot.command()
 async def test(ctx, arg):
+    await ctx.trigger_typing()
     await ctx.send(arg)
 
 @bot.command()
 async def greet(ctx, arg):
     """Have me greet the mentioned user!"""
+    await ctx.trigger_typing()
     await ctx.send(f"Hello {arg}!")
 
 @bot.command()
 async def hello(ctx):
     """Greet yourself!"""
+    await ctx.trigger_typing()
     await ctx.send(f'Hello {ctx.author.mention}!')
 
 @bot.command()
 async def oops(ctx):
     """When you F*uck something up..."""
+    await ctx.trigger_typing()
     await ctx.send("What did you do wrong this time...")
 
 @bot.command()
 async def optimist(ctx):
     """When you are having a bad day..."""
+    await ctx.trigger_typing()
     await ctx.author.send(random.choice(happy))
 
 @commands.command()
@@ -139,6 +155,7 @@ async def purge(ctx: commands.Context, limit: int = 100) -> None:
 @bot.command()
 async def color(ctx):
     """Get a random color in RGB values!"""
+    await ctx.trigger_typing()
     await ctx.send('Here are three numbers for RGB colors...')
     thing = random.randint(1, 255)
     await ctx.send(thing)
@@ -150,6 +167,7 @@ async def color(ctx):
 @bot.command()
 async def rps(ctx, arg):
     """Rock Paper Scissors!"""
+    await ctx.trigger_typing()
     rps2 = random.choice(rps1)
     await ctx.send(f"You chose {arg} and I chose {rps2}")
     if arg == "Scissors":
