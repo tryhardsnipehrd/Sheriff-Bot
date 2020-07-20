34e634bdcbddf6b55172bfa75ce9e19e99400734
tryhardsnipehrd <57113411+tryhardsnipehrd@users.noreply.github.com>
2020-07-19 18:55:33 -0600

    Update and rename Badcop.py to speedrun.py

--- speedrun.py
+++ speedrun.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-class Badcop(commands.Cog):
+class speedrun(commands.Cog):
     """SimpleCog"""
 
     def __init__(self, bot):
@@ -37,4 +37,4 @@ class Badcop(commands.Cog):
 # The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case SimpleCog.
 # When we load the cog, we use the name of the file.
 def setup(bot):
-    bot.add_cog(Badcop(bot))
+    bot.add_cog(speedrun(bot))
