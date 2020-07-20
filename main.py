6f462c419238abb9652ce825b7d186169b8668bc
tryhardsnipehrd <57113411+tryhardsnipehrd@users.noreply.github.com>
2020-07-19 20:13:33 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+import discord
+from discord.ext import commands
+
+
+"""A simple cog example with simple commands. Showcased here are some check decorators, and the use of events in cogs.
+For a list of inbuilt checks:
+http://dischttp://discordpy.readthedocs.io/en/rewrite/ext/commands/api.html#checksordpy.readthedocs.io/en/rewrite/ext/commands/api.html#checks
+You could also create your own custom checks. Check out:
+https://github.com/Rapptz/discord.py/blob/master/discord/ext/commands/core.py#L689
+For a list of events:
+http://discordpy.readthedocs.io/en/rewrite/api.html#event-reference
+http://discordpy.readthedocs.io/en/rewrite/ext/commands/api.html#event-reference
+"""
+
+
+class main(commands.Cog):
+    """SimpleCog"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    
+  
+
+    @commands.command(name='admin')
+    @commands.has_role("Owner")
+    async def only_me(self, ctx):
+        """A simple command which only responds to the owner of the bot."""
+
+        await ctx.send(f'Hello {ctx.author.mention}. This command can only be used by you!!')
+    
+    @commands.command(name="delete")
+    async def delete(self, ctx):
+        await ctx.message.delete()
+
+    
+    @commands.Cog.listener()
+    async def on_member_ban(self, guild, user):
+        """Event Listener which is called when a user is banned from the guild.
+        For this example I will keep things simple and just print some info.
+        Notice how because we are in a cog class we do not need to use @bot.event
+        For more information:
+        http://discordpy.readthedocs.io/en/rewrite/api.html#discord.on_member_ban
+        Check above for a list of events.
+        """
+
+        print(f'sadly, {user.name}-{user.id} was banned from {guild.name}-{guild.id} for unknown reasons...')
+
+# The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case SimpleCog.
+# When we load the cog, we use the name of the file.
+def setup(bot):
+    bot.add_cog(main(bot))
