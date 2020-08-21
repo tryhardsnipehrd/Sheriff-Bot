e8b5d7fcb2896e3ca6543e3840d7cec0c63b5cc3
tryhardsnipehrd <57113411+tryhardsnipehrd@users.noreply.github.com>
2020-08-21 12:28:56 -0600

    Update Sheriff.py

--- Sheriff.py
+++ Sheriff.py
@@ -25,11 +25,6 @@ async def test(ctx):
     await ctx.trigger_typing()
     await ctx.send("yes")
 
-@bot.command()
-async def greet(ctx):
-    """Have me greet the mentioned user!"""
-    await ctx.trigger_typing()
-    await ctx.send("Hello!")
 
 @bot.command()
 async def hello(ctx):
@@ -39,7 +34,7 @@ async def hello(ctx):
 
 @bot.command()
 async def cogs(ctx):
-    """Greet yourself!"""
+    """What cogs are loaded..."""
     await ctx.trigger_typing()
     for i in bot.cogs.keys():
         await ctx.send(i)
@@ -48,25 +43,10 @@ async def cogs(ctx):
 @commands.command()
 @commands.has_permissions(administrator=True)
 async def purge(ctx: commands.Context, limit: int = 100) -> None:
-  """ Removes X messages in channel.(MOD ONLY) """
+  """ Removes 100 messages in channel.(MOD ONLY) """
   await ctx.channel.purge(limit=int(limit))
     
-@bot.command()
-@commands.has_permissions(administrator=True)
-async def setup(ctx, arg=None):
-    """setup bot for server"""
-    if arg.lower() == "ddlc":
-        if "ddlc" not in bot.cogs.keys():
-            bot.load_extension("ddlc")
-    elif arg.lower() == "speedrun":
-        if "speedrun" not in bot.cogs.keys():
-            bot.load_extension("speedrun")
-    elif arg.lower() == "main":
-        if "main" not in bot.cogs.keys():
-            bot.load_extension("main")
-    elif arg == None:
-        for i in bot.cogs.keys():
-            bot.unload_extension(i)
+
 
    
 @bot.command()
