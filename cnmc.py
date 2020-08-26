5662fdcfbe532d8df0dda352751426f2f82b79d6
tryhardsnipehrd <57113411+tryhardsnipehrd@users.noreply.github.com>
2020-08-26 12:14:01 -0600

    Update cnmc.py

--- cnmc.py
+++ cnmc.py
@@ -1,1 +1,18 @@
-print("Hello")
+import discord
+from discord.ext import commands
+
+
+class cnmc(commands.Cog):
+    """SimpleCog"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command("thingy")
+    async def thingy(self, ctx):
+      ctx.send("I am online and working")
+        
+        
+        
+def setup(bot):
+    bot.add_cog(ddlc(bot))
