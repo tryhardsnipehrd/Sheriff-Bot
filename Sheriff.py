3a924d5ecf154377c765b6b89d0e7f88965993c0
tryhardsnipehrd <57113411+tryhardsnipehrd@users.noreply.github.com>
2020-08-28 09:41:32 -0600

    Update Sheriff.py

--- Sheriff.py
+++ Sheriff.py
@@ -71,7 +71,7 @@ async def test(ctx):
         await msg.edit(content="Here are the results")
 
     # If the reacted emoji is the X:
-    elif reaction.emoji == "✖️" and user == ctx.message.author:
+    elif reaction.emoji == "✖️" and user != bot.user:
         # We delete the messages
         # await ini.delete()
         await msg.delete()
