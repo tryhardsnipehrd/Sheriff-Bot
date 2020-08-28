d31fd6aa85ffc6ab2a7594deb576a175a1e2bb9b
tryhardsnipehrd <57113411+tryhardsnipehrd@users.noreply.github.com>
2020-08-28 09:32:32 -0600

    Update Sheriff.py

--- Sheriff.py
+++ Sheriff.py
@@ -64,14 +64,14 @@ async def test(ctx):
     reaction, user = await bot.wait_for('reaction_add')
 
     # If the reacted emoji is the checkmark:
-    if reaction.emoji == "✔️" and bot.user != ctx.message.author:
+    if reaction.emoji == "✔️" and user == ctx.message.author:
         # Clear the reactions from the original message
         await msg.clear_reactions()
         # Edit it to display some other content
         await msg.edit(content="Here are the results")
 
     # If the reacted emoji is the X:
-    elif reaction.emoji == "✖️" and bot.user != ctx.message.author:
+    elif reaction.emoji == "✖️" and user == ctx.message.author:
         # We delete the messages
         # await ini.delete()
         await msg.delete()
