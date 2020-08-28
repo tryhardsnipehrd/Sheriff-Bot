4fff6cc5de71070973a69153503a68bb9bb3b780
tryhardsnipehrd <57113411+tryhardsnipehrd@users.noreply.github.com>
2020-08-28 09:27:32 -0600

    Update Sheriff.py

--- Sheriff.py
+++ Sheriff.py
@@ -53,6 +53,7 @@ async def test(ctx):
     
     # Put your to be reaction added emojis in an iterable:
     reactions = ('✔️', '✖️')
+    msg.clear_reactions()
     
     # If we want to add multiple emoji's to a message, we have to loop
     # over each emoji in the reactions tuple, and react with it to the msg:
@@ -74,6 +75,7 @@ async def test(ctx):
         # We delete the messages
         # await ini.delete()
         await msg.delete()
+        await ctx.send("no")
 
 
 #@bot.command()
