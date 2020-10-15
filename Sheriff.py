ae059736880bb494e5fef60cc040048ab9ef1e1f
tryhardsnipehrd <57113411+tryhardsnipehrd@users.noreply.github.com>
2020-10-15 08:01:00 -0600

    Update Sheriff.py

--- Sheriff.py
+++ Sheriff.py
@@ -62,7 +62,7 @@ async def help(ctx):
         embed.add_field(name="Written In",value="Python, Discord.PY",inline=False)
         embed.add_field(name="Misc. Commands",value="hello -- Greet yourself!")
         embed.add_field(name="ㅤ",value="rules -- Leave blank to see all the rules, and specify a number to see that specific rule")
-        embed.add_field(name="ㅤ",value="mods -- Send a message to the mods. ***Only use if necessary!!!*** Usage `$mail <who/what>`"inline=False)
+        embed.add_field(name="ㅤ",value="mods -- Send a message to the mods. ***Only use if necessary!!!*** Usage `$mail <who/what>`",inline=False)
         await ctx.send("This command is in development! Stay tuned!")
         await ctx.send(embed=embed)
 
