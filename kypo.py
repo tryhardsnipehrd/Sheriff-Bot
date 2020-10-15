b342311a1cdbc32d419c0602b76399bf121ed046
tryhardsnipehrd <57113411+tryhardsnipehrd@users.noreply.github.com>
2020-10-15 07:54:11 -0600

    Update kypo.py

--- kypo.py
+++ kypo.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 
-admin_helper = (597921286018170900, 292877604053188618)
+admin_helper = (597921286018170900, 292877604053188618, 654822384569942017)
 
 class kypo(commands.Cog):
     """SimpleCog"""
